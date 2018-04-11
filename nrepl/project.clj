8ace6e8b826d0a0874b6738c3ff28dcbce3ea656
Matt Kocubinski <mkocubinski@gmail.com>
2018-04-11 14:02:08 -0400

    kurosawa.nrepl version 0.1.0.

--- nrepl/project.clj
+++ nrepl/project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/kurosawa.nrepl "0.1.0-SNAPSHOT"
+(defproject org.purefn/kurosawa.nrepl "0.1.0"
   :description "The Kurosawa nREPL library."
   :url "https://github.com/PureFnOrg/kurosawa"
   :license {:name "Eclipse Public License"
