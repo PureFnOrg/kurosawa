7909547c2436393ededfb5233ae85e4eef610c51
Matt Kocubinski <mkocubinski@gmail.com>
2018-05-01 21:03:57 -0400

    Initial work on modules.

--- log/project.clj
+++ log/project.clj
@@ -1,11 +1,6 @@
-(defproject org.purefn/kurosawa.log "0.1.0"
+(defproject org.purefn/kurosawa.log "0.2.0-SNAPSHOT"
   :description "A Kurosawa library for logging."
-  :url "https://github.com/PureFnOrg/kurosawa"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [com.taoensso/timbre "4.10.0"]]
-    :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                    [com.stuartsierra/component "0.3.2"]
-                                    [com.stuartsierra/component.repl "0.2.0"]]
-                   :source-paths ["dev"]}})
+  :dependencies [[com.taoensso/timbre _]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace _]
+                                  [com.stuartsierra/component _]
+                                  [com.stuartsierra/component.repl _]]}})
--- web/project.clj
+++ web/project.clj
@@ -1,17 +1,11 @@
-(defproject org.purefn/kurosawa.web "0.1.0"
+(defproject org.purefn/kurosawa.web "0.2.0-SNAPSHOT"
   :description "The Kurosawa web library."
-  :url "https://github.com/PureFnOrg/kurosawa"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
-                 [com.stuartsierra/component "0.3.2"]
+  :dependencies [[org.clojure/clojure _]
+                 [com.stuartsierra/component _]
 
                  [org.immutant/web "2.1.7"
                   :exclusions [ch.qos.logback/logback-classic]]
                  [org.slf4j/log4j-over-slf4j "1.7.25"]
                  [com.fzakaria/slf4j-timbre "0.3.5"]
 
-                 [org.purefn/kurosawa.log "0.1.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [com.stuartsierra/component.repl "0.2.0"]]
-                   :source-paths ["dev"]}})
+                 [org.purefn/kurosawa.log :version]])
