511632885a704894781741b62f2a700e0f0e3cc6
Matt Kocubinski <mkocubinski@gmail.com>
2018-06-25 18:12:41 -0400

    Version 2.0.2

--- log/project.clj
+++ log/project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/kurosawa.log "2.0.2-SNAPSHOT"
+(defproject org.purefn/kurosawa.log "2.0.2"
   :plugins [[lein-modules "0.3.11"]]
   :description "A Kurosawa library for logging."
   :dependencies [[com.taoensso/timbre _]]
