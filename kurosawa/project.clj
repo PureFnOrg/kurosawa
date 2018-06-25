8ce6ec5ca6ac2ffaf96378f121f9bff017023906
Matt Kocubinski <mkocubinski@gmail.com>
2018-06-25 18:20:44 -0400

    Fix version.

--- kurosawa/project.clj
+++ kurosawa/project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/kurosawa "2.0.2"
+(defproject org.purefn/kurosawa "2.0.3-SNAPSHOT"
   :description "A catch-all project that brings in all Kurosawa libs."
   :plugins [[lein-modules "0.3.11"]]
   :packaging "pom"
