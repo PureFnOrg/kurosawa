90aecc26dee944fac0e89a95aec7f4b54be8828a
Matt Kocubinski <mkocubinski@gmail.com>
2019-01-03 14:50:15 -0500

    Add comment.

--- core/src/org/purefn/kurosawa/health.clj
+++ core/src/org/purefn/kurosawa/health.clj
@@ -1,4 +1,8 @@
-(ns org.purefn.kurosawa.health)
+(ns org.purefn.kurosawa.health
+  "Contains a health check protocol which ought to be implemented by components whose
+  state might be healthy or unhealthy.  Most commonly testing for the liveness of an
+  underlying datastore.  This could be useful with a Kubernetes liveness probe,
+  for instance.")
 
 (defprotocol HealthCheck
   (healthy? [this]))
