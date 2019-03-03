69d4639d2732efa822995958bb9560a3c3fce142
Matt Kocubinski <mkocubinski@gmail.com>
2019-03-02 23:11:38 -0500

    Use provided scope for nrepl deps.

--- nrepl/project.clj
+++ nrepl/project.clj
@@ -2,6 +2,7 @@
   :plugins [[lein-modules "0.3.11"]]
   :description "The Kurosawa nREPL library."
   :dependencies [[com.stuartsierra/component _]
-                 [com.taoensso/timbre _]
-                 [cider/cider-nrepl "0.18.0"]
-                 [nrepl "0.5.2"]])
+                 [com.taoensso/timbre _]]
+  :profiles {:provided
+             {:dependencies [[cider/cider-nrepl "0.18.0"]
+                             [nrepl "0.5.2"]]}})
