3da59f4440122717835b9f4c6b5dababfb51440a
Matt Kocubinski <mkocubinski@gmail.com>
2019-04-07 19:59:27 -0400

    First pass at fetching from environment.

--- core/src/org/purefn/kurosawa/config/env.clj
+++ core/src/org/purefn/kurosawa/config/env.clj
@@ -1,8 +1,29 @@
 (ns org.purefn.kurosawa.config.env
-  "Load configuration from environment variables.")
+  "Load configuration from environment variables."
+  (:require [clojure.string :as str]
+            [org.purefn.kurosawa.config.parse :as parse]))
+
+(defn- munge-name
+  [s]
+  (str/join
+   "-"
+   (-> (str/lower-case s)
+       (str/split #"_")
+       (rest))))
+
+(defn- env-variables*
+  []
+  (->> (System/getenv)
+       (map (juxt (comp str/lower-case key)
+                  (comp munge-name key)
+                  val))
+       (group-by (comp first #(str/split % #"_") first))
+       (into {})))
+
+(def env-variables (memoize env-variables*))
 
 (defn fetch-config
   [name]
-  ;; this function is a stub, for now.
-  )
-
+  (some->> (get (env-variables) name)
+           (map (juxt second (comp parse/value last)))
+           (into {})))
