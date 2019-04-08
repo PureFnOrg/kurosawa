35b30035a48fac0c7ed9783bdd6ae31c8b1b0117
Matt Kocubinski <mkocubinski@gmail.com>
2019-04-08 15:29:48 -0400

    Clarifying docstrings.

--- core/src/org/purefn/kurosawa/config.clj
+++ core/src/org/purefn/kurosawa/config.clj
@@ -12,20 +12,33 @@
 (def ^:private config-map
   (atom nil))
 
+(defn set
+  "Set the `config-map` atom to configuration sourced from the envinronment,
+  probably from:
+
+  - `org.purefn.kurosawa.config.file/fetch`
+  - `org.purefn.kurosawa.config.env/fetch`, or
+  - `org.purefn.kurosawa.aws.ssm/fetch`"
+  [m]
+  (reset! config-map m))
+
 (defn default-config
   "This is our default, precendence based, load config from environment
   mechasnism.  A shallow merge was chosen to make final merged config map easier
-  to reason about."
+  to reason about.  Current precendence is:
+
+  1) Environemt variables
+  2) AWS SSM Paramter Store
+  3) The filesystem (legacy)
+
+  Ultimately we shouldn't need this, the config stage of application/development
+  startup needs to revisited.  But until all of our components' constructors are
+  refactored this isn't possible."
   []
-  (merge (file/fetch "etc/")
+  (merge (file/fetch "/etc/")
          (ssm/fetch (or (ssm/prefix-from-security-group)
                         "/local/platform"))
          (env/fetch)))
-
-(defn reset
-  "Reset the `config-map` atom to something other than the default."
-  [m]
-  (reset! config-map m))
 
 (defn fetch
   ([]
