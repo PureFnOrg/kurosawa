72e90e07d391215d2314ef547a6421cd5274e2f4
Matt Kocubinski <mkocubinski@gmail.com>
2019-04-09 15:29:30 -0400

    Version 2.1.1

--- core/project.clj
+++ core/project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/kurosawa.core "2.1.1-SNAPSHOT"
+(defproject org.purefn/kurosawa.core "2.1.1"
   :plugins [[lein-modules "0.3.11"]]
   :description "The root Kurosawa library."
   :dependencies [[com.stuartsierra/component _]
