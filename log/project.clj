50a89ad9e192eae3ea846afed85a2c36f6562405
Matt Kocubinski <mkocubinski@gmail.com>
2020-10-27 16:21:57 -0500

    Version 2.1.12

--- log/project.clj
+++ log/project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/kurosawa.log "2.1.12-SNAPSHOT"
+(defproject org.purefn/kurosawa.log "2.1.12"
   :plugins [[lein-modules "0.3.11"]]
   :description "A Kurosawa library for logging."
   :dependencies [[com.taoensso/timbre _]]
