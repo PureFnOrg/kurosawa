a71e9eedd0df0c34d53b9ae79229f5af54172400
Matt Kocubinski <mkocubinski@gmail.com>
2021-07-26 16:35:43 +0000

    Introduce a stateful prometheus component

--- web/src/org/purefn/kurosawa/web/prometheus.clj
+++ web/src/org/purefn/kurosawa/web/prometheus.clj
@@ -6,9 +6,12 @@
   requiring this namespace will break unless you've done so yourself."
   (:require [bidi.bidi :as bidi]
             [clojure.string :as str]
+            [clojure.spec.alpha :as s]
             [iapetos.core :as prometheus]
             [iapetos.collector.ring :as ring]
-            [taoensso.timbre :as log]))
+            [com.stuartsierra.component :as component]
+            [taoensso.timbre :as log])
+  (:import [iapetos.registry IapetosRegistry]))
 
 (defn path
   [routes ignore-keys {:keys [uri request-method]}]
@@ -31,3 +34,39 @@
   ([handler routes registry {:keys [ignore-keys] :as options}]
    (let [path-fn (partial path routes ignore-keys)]
      (ring/wrap-metrics handler registry {:path-fn path-fn}))))
+
+
+(defrecord PrometheusRegistry
+    [registration]
+
+  component/Lifecycle
+  (start [this]
+    (log/info "Starting PrometheusRegistry")
+    (assoc
+     this
+     :registry
+     (reduce
+      (fn [reg f]
+        (let [reg (f reg)]
+          (when-not (instance? IapetosRegistry reg)
+            (throw (ex-info (str "Registration fn passed to PrometheusRegistry must "
+                                 "return an instance of IapetosRegistry"
+                                 {:returned reg}))))
+          reg))
+      (-> (prometheus/collector-registry)
+          (ring/initialize))
+      registration)))
+
+  (stop [this]
+    this))
+
+(s/def ::registration (s/coll-of fn?))
+
+(defn create-registry
+  "Creates PrometheusRegistry which stuartsierra.component knows how to start.
+
+  `registration` must be a list of 1-arity functions expecting and returning an
+  `IaopetosRegistry`.  Each fn will be called on startup to register Prometheus metrics."
+  [registration]
+  {:pre [(s/assert* ::registration registration)]}
+  (PrometheusRegistry. registration))
