b1ba1d62295f57a8ed96b0ccbf90952ab1f76640
Matt Kocubinski <mkocubinski@gmail.com>
2021-09-21 12:06:34 -0500

    Update default for Prometheus ring histogram buckets

--- web/src/org/purefn/kurosawa/web/prometheus.clj
+++ web/src/org/purefn/kurosawa/web/prometheus.clj
@@ -42,7 +42,9 @@
                                  {:returned reg}))))
           reg))
       (-> (prometheus/collector-registry)
-          (ring/initialize))
+          (ring/initialize
+           {:latency-histogram-buckets [0.001 0.005 0.01 0.02 0.05 0.1
+                                        0.2 0.3 0.5 0.75 1 5 10 30]}))
       registration)))
 
   (stop [this]
