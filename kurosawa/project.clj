2011fa1fa696d902a68a3db8946999b8a07f4cde
Shantanu Kumar <kumar.shantanu@gmail.com>
2021-11-09 22:47:23 +0530

    Version 2.1.17-SNAPSHOT

--- kurosawa/project.clj
+++ kurosawa/project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/kurosawa "2.1.16"
+(defproject org.purefn/kurosawa "2.1.17-SNAPSHOT"
   :description "A catch-all project that brings in all Kurosawa libs."
   :plugins [[lein-modules "0.3.11"]]
   :packaging "pom"
--- log/project.clj
+++ log/project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/kurosawa.log "2.1.16"
+(defproject org.purefn/kurosawa.log "2.1.17-SNAPSHOT"
   :plugins [[lein-modules "0.3.11"]]
   :description "A Kurosawa library for logging."
   :dependencies [[com.taoensso/timbre _]]
