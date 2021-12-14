06ab0492dd627396af9445e869ddfdd4e61f2c56
Shantanu Kumar <kumar.shantanu@gmail.com>
2021-12-15 01:54:37 +0530

    Version 2.1.22

--- nrepl/project.clj
+++ nrepl/project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/kurosawa.nrepl "2.1.22-SNAPSHOT"
+(defproject org.purefn/kurosawa.nrepl "2.1.22"
   :plugins [[lein-modules "0.3.11"]]
   :description "The Kurosawa nREPL library."
   :dependencies [[com.stuartsierra/component _]
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/kurosawa "2.1.22-SNAPSHOT"
+(defproject org.purefn/kurosawa "2.1.22"
   :description "Parent for all that is Kurosawa"
   :plugins [[lein-modules "0.3.11"]]
 
