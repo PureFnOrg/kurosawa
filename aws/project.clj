21ab2ff83d7879d9b938b3a4e820b6090f0f28c0
Shantanu Kumar <kumar.shantanu@gmail.com>
2021-12-16 20:48:31 +0530

    Version 2.1.23

--- aws/project.clj
+++ aws/project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/kurosawa.aws "2.1.23-SNAPSHOT"
+(defproject org.purefn/kurosawa.aws "2.1.23"
   :plugins [[lein-modules "0.3.11"]]
   :description "AWS Utilities."
   :dependencies [[com.amazonaws/aws-java-sdk-core "1.11.533"]
