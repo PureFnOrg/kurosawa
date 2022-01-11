5ba98996d77dceaf4fc088e0b5b140cec280e476
Matt Kocubinski <mkocubinski@gmail.com>
2022-01-11 12:00:21 -0600

    Version 2.1.24

--- aws/project.clj
+++ aws/project.clj
@@ -1,4 +1,4 @@
-(defproject org.purefn/kurosawa.aws "2.1.24-SNAPSHOT"
+(defproject org.purefn/kurosawa.aws "2.1.24"
   :plugins [[lein-modules "0.3.11"]]
   :description "AWS Utilities."
   :dependencies [[com.amazonaws/aws-java-sdk-core "1.11.533"]
